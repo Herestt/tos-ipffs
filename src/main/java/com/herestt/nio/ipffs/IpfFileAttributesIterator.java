ad91eb3d37ea7cd1a4a56353b0dea744775de54b
Herestt <herestt@gmail.com>
2015-03-30 04:03:22 +0200

    IPF file attributes iterator init method implementation.

--- src/main/java/com/herestt/nio/ipffs/IpfFileAttributesIterator.java
+++ src/main/java/com/herestt/nio/ipffs/IpfFileAttributesIterator.java
@@ -1,10 +1,22 @@
 package com.herestt.nio.ipffs;
 
+import java.io.IOException;
+import java.nio.ByteOrder;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.DirectoryStream.Filter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.herestt.common.io.FileContent;
 
 public class IpfFileAttributesIterator extends IpfFileListIterator<IpfFileAttributes> {
 
+	private final static int HEADER_SIZE = 24;
+	
+	private int fileCount;
+	private int currentCount = 0;
+	private long listOffset;
+	
 	protected IpfFileAttributesIterator(IpfPath dir, SeekableByteChannel sbc,
 			Filter<IpfFileAttributes> filter) {
 		super(dir, sbc, filter);
@@ -12,8 +24,17 @@ public class IpfFileAttributesIterator extends IpfFileListIterator<IpfFileAttrib
 
 	@Override
 	public void init(IpfPath dir, SeekableByteChannel sbc) {
-		// TODO Auto-generated method stub
-		
+		Path ipf = dir.getFileSystem().getFileSystemPath();
+		try {
+			long headerOffset = Files.size(ipf) - HEADER_SIZE;
+			FileContent.access(sbc, headerOffset);
+			FileContent.order(ByteOrder.LITTLE_ENDIAN);
+			fileCount = FileContent.read().asUnsignedShort();
+			listOffset = FileContent.read().asUnsignedInt();
+			FileContent.position(listOffset);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
