7b2c6e23b171488e4ed59044466720a9f6cda2e2
Herestt <herestt@gmail.com>
2015-04-07 00:24:45 +0200

    IPF file store documentation updating.

--- src/main/java/com/herestt/nio/ipffs/IpfFileStore.java
+++ src/main/java/com/herestt/nio/ipffs/IpfFileStore.java
@@ -5,21 +5,34 @@ import java.nio.file.FileStore;
 import java.nio.file.attribute.FileAttributeView;
 import java.nio.file.attribute.FileStoreAttributeView;
 
+/**
+ * Describes the information data about an <code>.ipf</code> file.
+ * 
+ * @author Herestt
+ *
+ */
 public class IpfFileStore extends FileStore {
 
+	/** The <code>.ipf</code> file.*/
 	private String name;
-	private long fileCount;
-	private long listOffset;
-	private byte[] magicNumber;
 	
-	protected IpfFileStore(String name, long fileCount,
-			long listOffset, byte[] magicNumber) {
+	/** The count of files stored in the file system. */
+	private long fileCount;
+	
+	/** Beginning offset of the file list. */
+	private long listOffset;
+	
+	/** The magic number that identifies <code>.ipf</code> files*/
+	private byte[] magicNumber;
+
+	protected IpfFileStore(String name, long fileCount, long listOffset,
+			byte[] magicNumber) {
 		this.name = name;
 		this.fileCount = fileCount;
 		this.listOffset = listOffset;
 		this.magicNumber = magicNumber;
 	}
-	
+
 	@Override
 	public String name() {
 		return name;
@@ -80,17 +93,32 @@ public class IpfFileStore extends FileStore {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	/*** IPF File Store Specific Methods ***/
-	
+
+	/**
+	 * Gets the count of files held by this store.
+	 * 
+	 * @return the file count.
+	 */
 	public long getFileCount() {
 		return fileCount;
 	}
-	
+
+	/**
+	 * Gets the offset where begins the file description list. 
+	 * 
+	 * @return the file list offset.
+	 */
 	public long getListOffset() {
 		return listOffset;
 	}
-	
+
+	/**
+	 * Gets the magic number for IPF file systems.
+	 * 
+	 * @return the magic number.
+	 */
 	public byte[] getMagicNumber() {
 		return magicNumber;
 	}
